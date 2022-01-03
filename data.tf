0db8861aaca87243fd71086635bdd25bd9a375e8
brpo01 <opraise139@gmail.com>
2022-01-03 19:50:44 +0100

    added eks cluster resource to data.tf

--- data.tf
+++ data.tf
@@ -3,3 +3,12 @@ data "aws_availability_zones" "available_azs" {
     state = "available"
 }
 data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
+
+# get EKS cluster info to configure Kubernetes and Helm providers
+data "aws_eks_cluster" "cluster" {
+    name = module.eks-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+    name = module.eks-cluster.cluster_id
+}
